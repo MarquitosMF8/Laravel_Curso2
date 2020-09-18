a707ba60f17a3a79ddf9e212001172acf579c100
Marco Venegas <mvenegas@estrasol.com.mx>
2020-09-18 13:59:11 -0600

    #15 Autenticación [Rutas protegidas]

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::resource('/notas', 'NotaController');
+
+Route::get('/home', 'HomeController@index')->name('home');
